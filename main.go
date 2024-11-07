b9bfb0d8e2a337c82828223d0cde2a39013c91c0
Jérôme Mourey <>
2024-11-07 11:57:56 +0100

    fix args not read correctly, plus add free of rigth mp3 file to test

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 )
 
 func main() {
-	if err := Run(os.Args); err != nil {
+	if err := Run(os.Args[1:]); err != nil {
 		fmt.Fprintln(os.Stderr, "failed", err)
 		os.Exit(1)
 	}
@@ -23,17 +23,22 @@ func Run(args []string) error {
 	entry := "."
 	output := "db.sqlite"
 	columns := "Album/Movie/Show title;Artist;Title;Year"
+	ext := ".mp3"
 
 	fs := flag.NewFlagSet("arg", flag.ContinueOnError)
 	fs.StringVar(&entry, "i", entry, "input folder to scrap")
 	fs.StringVar(&output, "o", output, "output database")
-	fs.StringVar(&columns, "c", columns, "data to scrap")
+	fs.StringVar(&columns, "c", columns, "data to scrap spearated by coma")
+	fs.StringVar(&ext, "e", ext, "filter file by extension separated by coma")
 
 	err := fs.Parse(args)
 	if err != nil {
 		fs.PrintDefaults()
 		return err
 	}
+
+	fmt.Println(args, entry, output, columns)
+
 	ctx := context.Background()
 	db, err := model.Open(ctx, output, strings.Split(columns, ";"))
 	if err != nil {
@@ -41,7 +46,18 @@ func Run(args []string) error {
 	}
 	defer db.Close()
 
+	extensions := strings.Split(ext, ";")
+
 	return walk.Walk(entry, func(path string) error {
+		found := false
+		for _, e := range extensions {
+			if strings.HasSuffix(path, e) {
+				found = true
+			}
+		}
+		if !found {
+			return nil
+		}
 		tag, err := id3v2.Open(path, id3v2.Options{Parse: true})
 		if err != nil {
 			return fmt.Errorf("cannot open file : %w", err)
@@ -55,6 +71,7 @@ func Run(args []string) error {
 		for _, c := range strings.Split(columns, ";") {
 			f.Fields[c] = tag.GetTextFrame(tag.CommonID(c)).Text
 		}
+		fmt.Println(entry, f)
 		err = model.Upsert(db, ctx, []model.File{f})
 		if err != nil {
 			return fmt.Errorf("cannot upsert file %s : %w", path, err)
